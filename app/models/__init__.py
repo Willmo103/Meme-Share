2312d3d20be437adf3d74fbc2645169ed759ce2d
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2023-07-20 02:09:36 -0500

    adding group, save, updated meme models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 # Import all models from app\models\__init__.py
 
 from .user import User as User
+from .meme import Meme as Meme
---
+++ app/models/meme.py
@@ -0,0 +1,68 @@
+from PIL import Image
+from app import db
+from datetime import datetime
+from sqlalchemy import or_
+import os
+
+_upload_folder = os.environ.get("UPLOAD_FOLDER")
+
+
+class Meme(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    date_posted: datetime = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow
+    )
+    posted_by: int = db.Column(
+        db.Integer, db.ForeignKey("user.id"), nullable=True, default=None
+    )
+    title: str = db.Column(db.String(100), nullable=False)
+    url: str = db.Column(db.String(100), nullable=True, default=None)
+    filename: str = db.Column(db.String(100), nullable=True, default=None)
+    filepath: str = db.Column(db.String(100), nullable=True, default=None)
+    thumbnail: str = db.Column(db.String(100), nullable=True, default=None)
+    thumbnail_path: str = db.Column(db.String(100), nullable=True, default=None)
+    deleted: bool = db.Column(db.Boolean, nullable=False, default=False)
+    private: bool = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(
+        self,
+        posted_by: int,
+        title: str,
+        filename: str,
+        private: bool) -> None:
+            self.posted_by = posted_by
+            self.title = title
+            self.filename = filename
+            self.filepath = os.path.join(_upload_folder, filename)
+            self.thumbnail = f"thumbnail_{filename}"
+            self.thumbnail_path = os.path.join(_upload_folder, "thumbnails", self.thumbnail)
+            self.private = private
+            self.create_thumbnail()
+
+    def __repr__(self) -> str:
+        return f"Meme('{self.id}', '{self.filename}')"
+
+    def create_thumbnail(self, size=(128, 128)):
+        """Create a thumbnail of the meme."""
+        img = Image.open(self.filepath)
+        img.thumbnail(size)
+        img.save(self.thumbnail_path)
+
+    @classmethod
+    def from_url(cls, url, posted_by, title, private):
+        """Create a meme from a URL."""
+        # This method should download the image from the URL, save it to the
+        # upload folder, and then create a Meme object with the filename of
+        # the saved image.
+        # ...code to download image...
+        filename = ...  # the filename of the downloaded image
+        return cls(posted_by, title, filename, private)
+
+    @classmethod
+    def from_upload(cls, file, posted_by, title, private):
+        """Create a meme from an uploaded file."""
+        # This method should save the uploaded file to the upload folder,
+        # and then create a Meme object with the filename of the saved file.
+        # ...code to save uploaded file...
+        filename = ...  # the filename of the saved file
+        return cls(posted_by, title, filename, private)
