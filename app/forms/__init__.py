485761a01b49166ec86943fbc178cd59e4be3ed6
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-07-22 22:45:11 -0500

    admin form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,4 @@
 from .login_form import LoginForm as LoginForm
 from .register_form import RegistrationForm as RegistrationForm
 from .upload_meme_form import UploadMemeForm as UploadMemeForm
+from .admin_login_form import AdminLoginForm as AdminLoginForm
