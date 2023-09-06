973d9447dc5b1fff214f72277b2af35c77026f60
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2023-09-06 00:32:41 -0500

    fixed my F**k Up Oops IDK How that happened

---
+++ app/routes/index.py
@@ -0,0 +1,17 @@
+from flask import redirect, url_for, render_template
+from flask_login import current_user
+from . import endpoint
+from app.models import Meme
+import os
+
+
+@endpoint.route("/")
+@endpoint.route("/index", methods=["GET"])
+def index_page():
+    if not current_user.is_authenticated:
+        return redirect(url_for("routes.login"))
+    user_id = current_user.id
+    memes = Meme.query.filter_by(deleted=False).all()
+    return render_template(
+        "index.jinja", user_id=user_id, title="Intragram", memes=memes
+    )
